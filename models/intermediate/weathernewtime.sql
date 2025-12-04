70b321846ed82c57655314744c07ebb9b25dcd75
thomas dubar <totar.dubar@gmail.com>
2025-12-04 10:30:41 +0000

    restructuration

--- models/intermediate/weathernewtime.sql
+++ models/intermediate/weathernewtime.sql
@@ -1,17 +1,6 @@
 SELECT *,
-CASE 
-  WHEN CAST(LEFT(time_hhmm, 2) AS INT64) < 22 
-    THEN CONCAT(
-           CAST(LEFT(time_hhmm, 2) AS INT64) + timediff,
-           RIGHT(time_hhmm, 3)
-         )
+       FORMAT_DATETIME('%H:%M',
+           DATETIME_ADD(datetime_utc, INTERVAL timediff HOUR)
+       ) AS time_hhmm_transfo
 
-  WHEN CAST(LEFT(time_hhmm, 2) AS INT64) = 22 
-    THEN CONCAT("00", RIGHT(time_hhmm, 3))
-
-  WHEN CAST(LEFT(time_hhmm, 2) AS INT64) = 23 
-    THEN CONCAT("01", RIGHT(time_hhmm, 3))
-
-  ELSE "error"
-END AS time_hhmm_transfo
 FROM {{ ref('weather_addtimediff') }}
